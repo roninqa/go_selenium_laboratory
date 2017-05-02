a8eaaf7e6f0a758dca71c08ab6201f97902dd27d
cher1 <chue.her@autotrader.com>
2017-05-02 07:01:37 -0400

    b/c i'm happy

--- 06_pages/WelcomeToTheInternet_test.go
+++ 06_pages/WelcomeToTheInternet_test.go
@@ -16,10 +16,12 @@ func TestShouldBeOnTheCorrectPage(t *testing.T) {
 	defer driver.Quit()
 
 	driver.Get("http://the-internet.herokuapp.com/")
-	h1, _ := driver.FindElement(selenium.ByTagName, "h1")
-	if true, err := h1.Text(); err == nil {
-		t.Log("PASSED", true)
+	h1, err := driver.FindElement(selenium.ByTagName, "h1")
+	if err != nil {
+		t.Errorf("ERROR: Cannot find h1: %v\n", err)
+	} else if context, _ := h1.Text(); context == "Welcome to the Internet" {
+		t.Logf("PASSED: The context of h1: %v\n", context)
 	} else {
-		t.Errorf("FAILED: H1 is not present: %s\n", err)
+		t.Errorf("ERROR: The context does not match.")
 	}
 }
